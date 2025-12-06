1c0d923b6ee3dea48c285f75afe6d57232459bcc
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-06 22:11:56 +0530

    dockerize

--- src/main/java/com/saiteja/quizmonolithic/controller/QuizController.java
+++ src/main/java/com/saiteja/quizmonolithic/controller/QuizController.java
@@ -5,7 +5,6 @@ import com.saiteja.quizmonolithic.model.QuestionWrapper;
 import com.saiteja.quizmonolithic.model.Response;
 import com.saiteja.quizmonolithic.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
